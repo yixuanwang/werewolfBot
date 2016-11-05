f5496e719db250665bd4234e11bfc07148067da3
Kevin Sun <Kevin Sun>
2016-11-05 18:22:10 -0400

    .

--- index.js
+++ index.js
@@ -97,7 +97,7 @@ app.post('/webhook/', function (req, res) {
         if (event.postback) {
                 
                 let text = JSON.stringify(event.postback)                     
-                console.log("Postback received: " + JSON.stringify(event.postback));
+                sendTextMessage(sender,text);
                 continue;
             }
     }
