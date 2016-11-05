aaef6bad83f257fda0e5fb77f6b62e5896d55913
Kevin Sun <Kevin Sun>
2016-11-05 01:24:46 -0400

    updated index

---
+++ button.js
@@ -0,0 +1,57 @@
+import test from '../index';
+function sendGenericMessage(recipientId) {
+  var messageData = {
+    "recipient": {
+    "id": "RECIPIENT_ID"
+  },
+  "message": {
+    "attachment": {
+      "type": "template",
+      "payload": {
+        "template_type": "generic",
+        "elements": [
+          {
+            "title": "Swipe left/right for more options.",
+            "buttons": [
+              {
+                "type": "postback",
+                "title": "Button 1",
+                "payload": "button1"
+              },
+              {
+                "type": "postback",
+                "title": "Button 2",
+                "payload": "button2"
+              },
+              {
+                "type": "postback",
+                "title": "Button 3",
+                "payload": "button3"
+              }
+            ]
+          },
+          {
+            "title": "Swipe left/right for more options.",
+            "buttons": [
+              {
+                "type": "postback",
+                "title": "Button 4",
+                "payload": "button4"
+              },
+              {
+                "type": "postback",
+                "title": "Button 5",
+                "payload": "button5"
+              },
+              {
+                "type": "postback",
+                "title": "Button 6",
+                "payload": "button6"
+              }
+            ]
+          }
+        ]
+      }
+    }
+  }
+}
