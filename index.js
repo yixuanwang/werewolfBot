cd6e4dd496ae668341cbcb8722b98788293efbe9
rootluo <masterroot@icloud.com>
2016-11-05 04:25:20 -0400

    change the import

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 'use strict'
+function loadScript(url, callback)
+{
+    // Adding the script tag to the head as suggested before
+    var head = document.getElementsByTagName('head')[0];
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = url;
+
+    // Then bind the event to the callback function.
+    // There are several events for cross browser compatibility.
+    script.onreadystatechange = callback;
+    script.onload = callback;
+
+    // Fire the loading
+    head.appendChild(script);
+}
+loadScript("functions.js", createGameRoom);
+
 
 const express = require('express')
 const bodyParser = require('body-parser')
