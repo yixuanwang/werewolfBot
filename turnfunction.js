c003d6283e055ee5a8f08a966a04191309c87604
rootluo <masterroot@icloud.com>
2016-11-05 12:33:31 -0400

    change the startgame fucntion and turn

--- turnfunction.js
+++ turnfunction.js
@@ -11,27 +11,10 @@ function randomsort(a, b) {
 var arr = [1, 2, 3, 4, 5];
 arr.sort(randomsort);
 
-function turn(sender, admin, turntext, players){
+// if admin == sender, then the game starts
+function startgame(sender, admin){
 	if (sender == admin){
-		for (i=0; i<players.length;i++){
-			let messageData = { text:turntext}
-		    request({
-		        url: 'https://graph.facebook.com/v2.6/me/messages',
-		        qs: {access_token:token},
-		        method: 'POST',
-		        json: {
-		            recipient: {id:players[i]},
-		            message: messageData,
-		        }
-		    }, function(error, response, body) {
-		        if (error) {
-		            console.log('Error sending messages: ', error)
-		        } else if (response.body.error) {
-		            console.log('Error: ', response.body.error)
-		        }
-		    })
-		};
-		generateRole(roles);
+		turn(GameRoom.players, turn1text)
 	} else {
 		let messageData = { text:"You are not the admin of the room." }
 	    request({
@@ -52,6 +35,27 @@ function turn(sender, admin, turntext, players){
 	}
 }
 
+function turn(players, turntext){
+ 			for (i=0; i<players.length;i++){
+			let messageData = { text:turntext}
+		    request({
+		        url: 'https://graph.facebook.com/v2.6/me/messages',
+		        qs: {access_token:token},
+		        method: 'POST',
+		        json: {
+		            recipient: {id:players[i]},
+		            message: messageData,
+		        }
+		    }, function(error, response, body) {
+		        if (error) {
+		            console.log('Error sending messages: ', error)
+		        } else if (response.body.error) {
+		            console.log('Error: ', response.body.error)
+		        }
+		    })
+		};
+		generateRole(roles);
+ }
 function generateRole(myArray){
 	var randarray = myArray;
 	for (i=0; i<players.length;i++){
