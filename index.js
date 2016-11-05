792742d80d9aa697cd8c817c1d5b631443d96fe1
Kevin Sun <Kevin Sun>
2016-11-05 10:41:17 -0400

    modified buttons function

--- index.js
+++ index.js
@@ -63,9 +63,12 @@ app.post('/webhook/', function (req, res) {
                 createGameRoom(sender);
                 continue;
             }
-
-            sendGenericMessage(sender);
+            if (text == "image"){
+                sendGenericMessage(sender);
+                continue;
+            }
             
+
             sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
             //if(text == "creategame") {
                 //let startBot = new events.EventEmitter(); 
@@ -146,26 +149,19 @@ function sendGenericMessage(sender) {
                 "elements": [{
                     "title": "First card",
                     "subtitle": "Element #1 of an hscroll",
-                    "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+                    //"image_url": "http://messengerdemo.parseapp.com/img/rift.png",
                     "buttons": [{
                         "type": "web_url",
                         "url": "https://www.messenger.com",
-                        "title": "web url"
+                        "title": "Kill someone"
                     }, {
                         "type": "postback",
                         "title": "Postback",
-                        "payload": "Payload for first element in a generic bubble",
+                        "payload": "Do nothing",
                     }],
-                }, {
-                    "title": "Second card",
-                    "subtitle": "Element #2 of an hscroll",
-                    "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
-                    "buttons": [{
-                        "type": "postback",
-                        "title": "Postback",
-                        "payload": "Payload for second element in a generic bubble",
-                    }],
-                }]
+                }, 
+
+                ]
             }
         }
     }
