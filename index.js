dd3f1579e11b7425269f26106d103e027559833c
Kevin Sun <Kevin Sun>
2016-11-05 17:12:46 -0400

    changed the button print

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ app.post('/webhook/', function (req, res) {
         }
         if (event.postback) {
                 let text = JSON.stringify(event.postback)
-                sendTextMessage(sender, token);
+                sendTextMessage(sender, text);
                 continue;
             }
     }
