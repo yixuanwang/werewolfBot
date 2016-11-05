148f02e7b5d2009dee82dfbab60980ff4e196a6f
yixuan <wangyixuan98@hotmail.com>
2016-11-05 03:11:02 -0400

    turn

--- turnfunction.js
+++ turnfunction.js
@@ -1,12 +1,17 @@
-import
-var players = []
-var roles = []
+import './data.js'
 
 Array.prototype.randomElement = function () {
     return this[Math.floor(Math.random() * this.length)]
 }
 
-function turn(sender, admin, turntext, numberofplayers){
+function randomsort(a, b) {
+    return Math.random()>.5 ? -1 : 1;
+}
+
+var arr = [1, 2, 3, 4, 5];
+arr.sort(randomsort);
+
+function turn(sender, admin, turntext, players){
 	if (sender == admin){
 		for (i=0; i<players.length;i++){
 			let messageData = { text:turntext}
@@ -26,7 +31,7 @@ function turn(sender, admin, turntext, numberofplayers){
 		        }
 		    })
 		};
-		generateRole();
+		generateRole(roles);
 	} else {
 		let messageData = { text:"You are not the admin of the room." }
 	    request({
@@ -37,10 +42,35 @@ function turn(sender, admin, turntext, numberofplayers){
 	            recipient: {id:sender},
 	            message: messageData,
 	        }
-	    }
+	    }, function(error, response, body) {
+		    if (error) {
+		        console.log('Error sending messages: ', error)
+		    } else if (response.body.error) {
+	            console.log('Error: ', response.body.error)
+	        }
+		})
 	}
 }
 
 function generateRole(myArray){
-	var rand = myArray.randomElement();
+	var randarray = myArray;
+	for (i=0; i<players.length;i++){
+		var rand = randarray.randomElement();
+		let messageData = { text:turntext}
+	    request({
+	        url: 'https://graph.facebook.com/v2.6/me/messages',
+	        qs: {access_token:token},
+	        method: 'POST',
+	        json: {
+	            recipient: {id:players[i]},
+	            message: messageData,
+	        }
+	    }, function(error, response, body) {
+	        if (error) {
+	            console.log('Error sending messages: ', error)
+	        } else if (response.body.error) {
+	            console.log('Error: ', response.body.error)
+	        }
+	    })
+	};
 }
