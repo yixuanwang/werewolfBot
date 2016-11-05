10ebe0f81698f80a9d4e0eedb0edfc9b26a2a019
yixuan <wangyixuan98@hotmail.com>
2016-11-05 16:50:50 -0400

    starting turn

--- turnfunction.js
+++ turnfunction.js
@@ -16,43 +16,13 @@ function startgame(sender, admin){
 	if (sender == admin){
 		turn(GameRoom.players, turn1text)
 	} else {
-		let messageData = { text:"You are not the admin of the room." }
-	    request({
-	        url: 'https://graph.facebook.com/v2.6/me/messages',
-	        qs: {access_token:token},
-	        method: 'POST',
-	        json: {
-	            recipient: {id:sender},
-	            message: messageData,
-	        }
-	    }, function(error, response, body) {
-		    if (error) {
-		        console.log('Error sending messages: ', error)
-		    } else if (response.body.error) {
-	            console.log('Error: ', response.body.error)
-	        }
-		})
+		
 	}
 }
 
 function turn(players, turntext){
  			for (i=0; i<players.length;i++){
-			let messageData = { text:turntext}
-		    request({
-		        url: 'https://graph.facebook.com/v2.6/me/messages',
-		        qs: {access_token:token},
-		        method: 'POST',
-		        json: {
-		            recipient: {id:players[i]},
-		            message: messageData,
-		        }
-		    }, function(error, response, body) {
-		        if (error) {
-		            console.log('Error sending messages: ', error)
-		        } else if (response.body.error) {
-		            console.log('Error: ', response.body.error)
-		        }
-		    })
+			
 		};
 		generateRole(roles);
  }
@@ -60,21 +30,6 @@ function generateRole(myArray){
 	var randarray = myArray;
 	for (i=0; i<players.length;i++){
 		var rand = randarray.randomElement();
-		let messageData = { text:turntext}
-	    request({
-	        url: 'https://graph.facebook.com/v2.6/me/messages',
-	        qs: {access_token:token},
-	        method: 'POST',
-	        json: {
-	            recipient: {id:players[i]},
-	            message: messageData,
-	        }
-	    }, function(error, response, body) {
-	        if (error) {
-	            console.log('Error sending messages: ', error)
-	        } else if (response.body.error) {
-	            console.log('Error: ', response.body.error)
-	        }
-	    })
+		
 	};
 }
