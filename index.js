592d89e176ef0b4162724558b906d8a796f3fb6e
rootluo <masterroot@icloud.com>
2016-11-06 02:27:53 -0500

    change the generateRole in index

--- index.js
+++ index.js
@@ -358,10 +358,37 @@ function turn(players, turntext){
         };
         generateRole(roles);
  }
-function generateRole(myArray){
-    var randarray = myArray;
-    for (i=0; i<players.length;i++){
-        var rand = randarray.randomElement();
-        
-    };
+ 
+function generateRole(roomid){
+    
+    if(gameRoomArray[roomid]) {
+        var tempPlayerArray = gameRoomArray[roomid].players;
+        var playerCount = tempPlayerArray.length;
+        var wolfCount = Math.floor(playerCount / 3);
+        var i;
+        var j;
+        var tempRandNum = [];
+        for(i = 0; i < wolfCount; i++) {
+            var rand = Math.floor((Math.random() * playerCount) );
+            if(tempRandNum.indexOf(rand) > -1) continue;
+            tempRandNum.push(rand);
+        }
+        for(i = 0; i < wolfCount; i++) {
+            let wolfUserID = tempPlayerArray[tempRandNum[i]];
+            for(j = 0; j < globalPlayer.length; j++) {
+                if(globalPlayer[j].id == wolfUserID) {
+                    globalPlayer[i].role = "wolf";
+                    break;
+                }
+            }
+        }
+        gameRoomArray[roomid].players = tempPlayerArray;
+    }
+    else {
+        console.log('Error in generateRole');
+    }
+    //var randarray = playersArray;
+    //for (i=0; i<players.length;i++){
+    //   var rand = randarray.randomElement();        
+    //};
 }
