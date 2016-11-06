806e46aa690631bae9b617c5d2f076c025e72e75
Kevin Sun <Kevin Sun>
2016-11-06 06:59:40 -0500

    .

--- index.js
+++ index.js
@@ -254,21 +254,7 @@ function sendNightOptions(sender) {
                 }
                 
             }
-            request({
-                    url: 'https://graph.facebook.com/v2.6/me/messages',
-                    qs: {access_token:token},
-                    method: 'POST',
-                    json: {
-                        recipient: {id:sender},
-                        message: messageData,
-                    }
-                }, function(error, response, body) {
-                    if (error) {
-                        console.log('Error sending messages: ', error)
-                    } else if (response.body.error) {
-                        console.log('Error: ', response.body.error)
-                    }
-             })
+            
         }
         
     }
