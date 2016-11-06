2f9354f7806c1e22d08530df9b6a7b770dad8c4b
rootluo <masterroot@icloud.com>
2016-11-06 10:53:09 -0500

    tsts

--- index.js
+++ index.js
@@ -99,6 +99,7 @@ app.post('/webhook/', function (req, res) {
             if (text.substring(0,4) == "eat ") {
                 let j;
                 var roomid;
+                sendTextMessage(sender, text.substring(4,11));
                 for(j = 0; j < globalPlayer.length; j++) {
                     if(sender == globalPlayer[j].id) {
                         roomid = globalPlayer[j].room;
