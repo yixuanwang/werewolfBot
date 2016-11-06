76e65cab8716e03ee5a906d09920c3ba6cc15d1e
rootluo <masterroot@icloud.com>
2016-11-05 22:07:14 -0400

    fixing

--- turnfunction.js
+++ turnfunction.js
@@ -12,7 +12,7 @@ var arr = [1, 2, 3, 4, 5];
 arr.sort(randomsort);
 
 
-function playerRearrage(roomid) {
+function playerRearrange(roomid) {
 	var i;
 	for(i = 0; i < gameRoomArray[roomid].players.length; i++) {
 		let tempPlayer = new player(gameRoomArray[roomid].players[i]);
@@ -24,19 +24,22 @@ function playerRearrage(roomid) {
 
 // if admin == sender, then the game starts
 function startgame(sender, roomid){
-	playerRearrage(roomid);
+	playerRearrange(roomid);
 	var i;
-	for (i=0; i<gameRoomArray[roomid].players.length; i++)
-		if (sender==gameRoomArray[roomid].players[0]){
+	for (i=0; i < gameRoomArray[roomid].players.length; i++) {
+		sendTextMessage(sender, "You are in the loop");
+
+		if (sender == gameRoomArray[roomid].players[0]){
 			turn(gameRoomArray[roomid].players, turn1text);
 		} else {
 			sendTextMessage(sender, "You are not the admin of the room "+ roomid);
 		}
+	}
 }
 
 function turn(players, turntext){
  	for (i=0; i<players.length;i++){
- 		senTextMessage(players[i], )
+ 		sendTextMessage(players[i], "Welcome");
 		};
 		generateRole(roles);
  }
