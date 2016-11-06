7827bf37fad1e3da8d4291d1c3edae1e9d70a607
rootluo <masterroot@icloud.com>
2016-11-06 01:23:40 -0400

    change the commmit function

--- command.js
+++ command.js
@@ -1,7 +1,41 @@
 
+// Help Handler
 var helpInstruction = "This is help Instruction, nothing new"
 function helpInfo(sender) {
 	sendTextMessage(sender, helpInstruction);
+}
+
+// Flee Handler
+function flee(sender) {
+	let i;
+	for(i = 0; i < globalPlayer.length; i++) {
+		if(sender == globalPlayer[i].id)  {
+			globalPlayer[i].alive = false;
+			sendTextMessage(sender, "You have successfully fled from the game");
+		}
+	}
+}
+
+// Call everyone in the room
+/******** I AM NOT SURE IF THIS WOULD WORK!!!!!  ********/
+function callEveryone(roomid, text) {
+    let messageData = { text:text }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+        	// I AM NOT SURE WITH THIS RECIPIENT ID
+            recipient: {id: gameRoomArray[roomid].players},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
 }
 
 
@@ -21,5 +55,5 @@ function command(sender, text) {
 		helpInfo(sender);
 	// command "flee #roomID"
 	else if(text.substring(0,5) == "flee ")
-		flee()
+		flee(sender);
 }
